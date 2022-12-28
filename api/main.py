336bf90beffb5f4b92ecff7494d02a6041a78ac1
sgorji <saeed.gorji@proton.me>
2022-12-28 21:27:32 +0100

    Enabled pylint, black and pycodestyle for python

--- api/main.py
+++ api/main.py
@@ -18,18 +18,17 @@ CORS(app)
 
 app.config["DEBUG"] = DEBUG
 
+
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
-    headers = {
-        "Accept-Version": "v1",
-        "Authorization": "Client-ID " + UNSPLASH_KEY
-    }
+    headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
     params = {"query": word}
-    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
-    
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params, timeout=1)
+
     data = response.json()
     return data
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
