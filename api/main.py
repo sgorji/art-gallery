6de2a730b0c63312366bc8693226305057642906
sgorji <saeed.gorji@proton.me>
2023-01-02 18:59:19 +0100

    Delete image api endpoint

--- api/main.py
+++ api/main.py
@@ -48,5 +48,18 @@ def images():
         return {"inserted_id": inserted_id}
 
 
+@app.route("/images/<image_id>", methods=["DELETE"])
+def delete_image(image_id):
+    if request.method == "DELETE":
+        # deleting the image with the given id from the DB
+        resp = images_collection.delete_one({"_id": image_id})
+
+        if resp:
+            if resp.deleted_count:
+                return {"deleted_id": image_id}
+            return {"error": "Image not found!"}, 404
+        return {"error": "Internal server error"}, 500
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
